9b649d07d06ada9e5f2b9984d40f18b80aa4f0d2
Gelst13 <Gelst13@rambler.ru>
2023-07-11 18:04:50 +0300

    package structure

--- timez/config.py
+++ timez/config.py
@@ -3,4 +3,4 @@ import os
 key = os.urandom(24)  # specify the length in brackets
 SECRET_KEY = key
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///tzr.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///timez.db'
--- timez/routes.py
+++ timez/routes.py
@@ -1,36 +1,8 @@
-import json
-from flask import Flask, redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from tzr_utils import TimeKeeper
+from flask import flash, redirect, render_template, request, url_for, abort
+from timez import app, db
+from timez.models import Contact
+from timez.tzr_utils import TimeKeeper
 
-
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-
-
-class Contacts(db.Model):
-    contact_name = db.Column(db.String(255), primary_key=True)
-    platform = db.Column(db.String(255), nullable=False)
-    comment = db.Column(db.String(255))
-    location = db.Column(db.String(255))
-    zone_name = db.Column(db.String(255))
-    utc_offset = db.Column(db.Float)
-
-    def __repr__(self):
-        return '<Contact {} from {}>'.format(self.contact_name, self.location)
-
-    @property
-    def contact_time(self):
-        if self.zone_name:
-            return TimeKeeper.get_current_time(self.zone_name)
-        elif self.utc_offset:
-            return TimeKeeper.get_current_time(self.utc_offset)
-
-
-# with app.app_context():
-#     db.create_all()
 
 new_contact = dict()
 
@@ -42,7 +14,7 @@ def index():
         result = TimeKeeper.time_operation_0(data)
         return "In {} hours {} minutes it'll be: {}".format(data[0], data[1], result)
     else:
-        contacts = Contacts.query.order_by(Contacts.contact_name).all()
+        contacts = Contact.query.order_by(Contact.contact_name).all()
         return render_template('index.html', contacts=contacts)
 
 
@@ -70,7 +42,7 @@ def time_operations():
 
 @app.route('/delete/<contact_name>')
 def delete(contact_name):
-    contact_to_delete = Contacts.query.get_or_404(contact_name)
+    contact_to_delete = Contact.query.get_or_404(contact_name)
     try:
         db.session.delete(contact_to_delete)
         db.session.commit()
@@ -90,26 +62,26 @@ def add_contact():
         zone_name, utc_offset = TimeKeeper.tz_from_input(info[4])
         print(zone_name, utc_offset)
         if zone_name:
-            new_contact = Contacts(contact_name=info[0], platform=info[1],
-                                   comment=info[2], location=info[3],
-                                   zone_name=zone_name)
+            new_contact = Contact(contact_name=info[0], platform=info[1],
+                                  comment=info[2], location=info[3],
+                                  zone_name=zone_name)
         elif utc_offset:
-            new_contact = Contacts(contact_name=info[0], platform=info[1],
-                                   comment=info[2], location=info[3],
-                                   utc_offset=utc_offset)
+            new_contact = Contact(contact_name=info[0], platform=info[1],
+                                  comment=info[2], location=info[3],
+                                  utc_offset=utc_offset)
         else:
-            new_contact = Contacts(contact_name=info[0], platform=info[1],
-                                   comment=info[2], location=info[3])
+            new_contact = Contact(contact_name=info[0], platform=info[1],
+                                  comment=info[2], location=info[3])
         db.session.add(new_contact)
         db.session.commit()
         new_contact = ()
-        return render_template('index.html')
+        return redirect(url_for('index'))
     return render_template('add_contact.html')
 
 
 @app.route('/update/<contact_name>', methods=['POST', 'GET'])
 def update(contact_name):
-    contact = Contacts.query.get_or_404(contact_name)
+    contact = Contact.query.get_or_404(contact_name)
     if request.method == 'POST':
         info = (request.form.get('contact_name'), request.form.get('platform'),
                 request.form.get('comment'), request.form.get('location'),
@@ -128,7 +100,3 @@ def update(contact_name):
             return f'There was an issue updating contact {contact_name}'
     else:
         return render_template('update.html', contact=contact)
-
-
-if __name__ == '__main__':
-    app.run()
