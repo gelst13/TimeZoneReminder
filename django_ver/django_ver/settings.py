45552032064210fcbb68df3aa6e069f5170f42dd
Gelst13 <Gelst13@rambler.ru>
2023-08-23 14:34:28 +0300

    Updated - django_ver/django_ver/settings.py

--- django_ver/django_ver/settings.py
+++ django_ver/django_ver/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'timop.apps.TimopConfig'
+    'timop.apps.TimopConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
