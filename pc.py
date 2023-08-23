66bf458730a55bf34b51f7eb6f26e76775f33d75
Gelst13 <Gelst13@rambler.ru>
2023-08-23 14:02:41 +0300

    Created - pc.py for tasks and pseudocode

---
+++ pc.py
@@ -0,0 +1,8 @@
+"""
+time operations page is main page for now,
+but later contacts page will become main page
+
+- add blog app with history of my application
+
+- maybe it'll create post automatically from blog.csv ?
+"""
