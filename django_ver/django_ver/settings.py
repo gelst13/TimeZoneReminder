700a2235a4b6e8be8d079e5feefa7a34b3c6c98e
Gelst13 <Gelst13@rambler.ru>
2023-08-23 16:23:03 +0300

    Updated - settings.py - crispy forms

--- django_ver/django_ver/settings.py
+++ django_ver/django_ver/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'timop.apps.TimopConfig',
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
@@ -124,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
