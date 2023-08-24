e1b9d7bfd49ce8b20a55d8cb0a3fa3c4890648e2
Gelst13 <Gelst13@rambler.ru>
2023-08-24 11:28:50 +0300

    Updated - django_ver/users/views.py create view for users’s profile

--- django_ver/users/views.py
+++ django_ver/users/views.py
@@ -15,3 +15,9 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+
