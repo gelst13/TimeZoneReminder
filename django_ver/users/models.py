6951c0fbe4e632c14bab851834d8407b8f779a24
Gelst13 <Gelst13@rambler.ru>
2023-09-07 11:29:51 +0300

    Added Contact book in users.models

--- django_ver/users/models.py
+++ django_ver/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 from PIL import Image
 
 
@@ -19,3 +20,21 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+
+class Contact(models.Model):
+    # id = models.IntegerField(primary_key=True)
+    contact_name = models.CharField(max_length=100)
+    platform = models.CharField(max_length=20)
+    comment = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    zone_name = models.CharField(max_length=100)
+    utc_offset = models.FloatField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __repr__(self):
+        return '<Contact {} from {}>'.format(self.contact_name,
+                                             self.location)
+
+    def get_absolute_url(self):
+        return reverse('contact-detail', kwargs={'pk': self.pk})
--- pc.py
+++ pc.py
@@ -3,8 +3,5 @@ time operations page is main page for now,
 but later users page will become main page
 ?? where should lay base.html?
 
-- change users app into users app
-
-- add blog app with history of my application
-- maybe it'll create post automatically from blog.csv ?
+- change Contact model: set zone_name to nullable=True
 """
