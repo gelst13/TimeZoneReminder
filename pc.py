2947b20e484deb71dd069b61af2c1d6a2701dd34
Gelst13 <Gelst13@rambler.ru>
2023-09-08 11:51:12 +0300

    Added task

--- pc.py
+++ pc.py
@@ -6,4 +6,6 @@ but later users page will become main page
 - change Contact model: set zone_name to nullable=True
 
 - change tzr_utils to work with Olsen tz names Asia/Almaty
+
+- add user's time zone to profile
 """
