225dc907f601f899ec53990a99461d2e2b3af320
Gelst13 <Gelst13@rambler.ru>
2023-09-14 12:45:18 +0300

    Updated pc.py

--- pc.py
+++ pc.py
@@ -1,8 +1,4 @@
 """
-time operations page is main page for now,
-but later users page will become main page
-?? where should lay base.html?
-
 - change Contact model: set zone_name to nullable=True
 
 - change tzr_utils to work with Olsen tz names Asia/Almaty
@@ -11,5 +7,6 @@ but later users page will become main page
 
 - correct how "author's" image is displayed in post_detail.html
 
+ideas:
 - add comments to posts from logged-in users
 """
