bc0021f8b35f89a65fdc5d7e3420e5428154a51d
Gelst13 <Gelst13@rambler.ru>
2023-09-15 12:22:52 +0300

    Updated pc.py

--- pc.py
+++ pc.py
@@ -1,6 +1,11 @@
 """
 - change Contact model: set zone_name to nullable=True
 
+- clean tzr_utils:
+    -- refactor 1st func with offset, 2nd func with zone name
+    -- make current existing functions smaller and clearer
+    -- delete functionality for CLI interface
+
 - change tzr_utils to work with Olsen tz names Asia/Almaty
 
 + add user's time zone to profile
@@ -8,6 +13,10 @@
 - correct how "author's" image is displayed in post_detail.html
 
 - use users's local tz in timop
+- change local tz for anonymous user:
+because in Django utility cannot grab local time correctly
+
+- display offset correctly as +00:30 or -02:00
 
 ideas:
 - add comments to posts from logged-in users
