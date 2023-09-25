9a08a6fa2579a5b7d7a9fed785211a1d6f849194
Gelst13 <Gelst13@rambler.ru>
2023-09-25 10:26:16 +0300

    Updated - django_ver/users/forms.py : changed ProfileUpdateForm

--- django_ver/users/forms.py
+++ django_ver/users/forms.py
@@ -24,4 +24,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image', 'utc_offset']
+        fields = ['image', 'offset']
